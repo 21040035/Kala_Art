87fa2e4886cd9cf4f86910a576fdced950dfe4cb
Smriti Ghale <117796038+21040035@users.noreply.github.com>
2024-04-17 18:22:46 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const crypto = require('crypto');
 
 app.use(cors({
-  origin: ['http://localhost:3000',],
+  origin: ['https://kala-art.vercel.app/',],
   credentials: true
 }));
 
