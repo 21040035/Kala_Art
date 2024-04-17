40cab07a4b564bd07e65ad33d059c76ce732c63f
Smriti Ghale <117796038+21040035@users.noreply.github.com>
2024-04-17 17:21:40 +0545

    server

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://kala-art-6a1o.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000";
+export const backend_url = "hhttps://kala-art-6a1o.vercel.app/";
